a72b4721653ab7bcc48cbca87c40a2627056a9ce
TungNT (Blue) <tungnt@9pay.vn>
2020-06-05 11:28:19 +0700

    Init

--- config.php
+++ config.php
@@ -6,7 +6,9 @@ return [
             'rsa' => [
                 'private_key' => env('TEST_SECURITY_RSA_PRIVATE_KEY'),
                 'public_key' => env('TEST_SECURITY_RSA_PUBLIC_KEY'),
-                'password' => env('TEST_SECURITY_RSA_PASSWORD')
+                'password' => env('TEST_SECURITY_RSA_PASSWORD'),
+                'message' => env('TEST_SECURITY_RSA_MESSAGE'),
+                'signature' => env('TEST_SECURITY_RSA_SIGNATURE'),
             ]
         ]
     ]
--- tests/Security/RSATest.php
+++ tests/Security/RSATest.php
@@ -6,9 +6,16 @@ namespace OneSite\Core\Test\Security;
 use OneSite\Core\Security\RSA;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class RSATest
+ * @package OneSite\Core\Test\Security
+ */
 class RSATest extends TestCase
 {
 
+    /**
+     * @var RSA
+     */
     private $service;
 
     /**
@@ -47,4 +54,35 @@ class RSATest extends TestCase
         return $this->assertTrue(true);
     }
 
+    /**
+     * PHPUnit test: vendor/bin/phpunit --filter testSign tests/Security/RSATest.php
+     */
+    public function testSign()
+    {
+        $signText = $this->service->sign(
+            config('test.security.rsa.private_key'),
+            config('test.security.rsa.signature'),
+            config('test.security.rsa.password')
+        );
+
+        echo "\n" . $signText;
+
+        return $this->assertTrue(true);
+    }
+
+    /**
+     * PHPUnit test: vendor/bin/phpunit --filter testVerify tests/Security/RSATest.php
+     */
+    public function testVerify()
+    {
+        $signVerify = $this->service->verify(
+            config('test.security.rsa.public_key'),
+            config('test.security.rsa.message'),
+            config('test.security.rsa.signature')
+        );
+
+        echo "\n" . ($signVerify ? 'Is valid' : 'Is not valid');
+
+        return $this->assertTrue($signVerify);
+    }
 }
